286cef14b952ba141327ddd75a5bcbe330e128de
golodniy alexandr <aleks.golodny@gmail.com>
2024-08-29 10:36:54 +0300

    [FEATURE] добавлен хук, вызван в нескольких метода

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import { useEffect } from "react";
 import "./App.css";
+import { useTelegram } from "../../Hooks/useTelegram";
 
 function App() {
+  const { onToggleButton, tg } = useTelegram();
+
   useEffect(() => {
     tg.ready();
   }, []);
 
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <button onClick={onToggleButton}>toggle</button>
+    </div>
+  );
 }
 
 export default App;
