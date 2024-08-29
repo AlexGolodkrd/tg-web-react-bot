c63781ed1149e2d3d0aad77be4089fc564e0d54b
golodniy alexandr <aleks.golodny@gmail.com>
2024-08-29 10:13:07 +0300

    [FEATURE] добавлена кнопка и хеддер для реакта

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
+import { useEffect } from "react";
 import "./App.css";
-const tg = window.Telegram.WebApp;
-function App() {
-  const onClose = () => {
-    tg.close();
-  };
-  const useEffect = () => {
-    tg.ready();
-  };
 
-  return (
-    <div className="App">
-      {" "}
-      <button onclick={onClose}>Close</button>
-    </div>
-  );
+function App() {
+  useEffect(() => {
+    tg.ready();
+  }, []);
+
+  return <div className="App"></div>;
 }
 
 export default App;
